a579fa80b24f91a4c3682677d0d42853fa95737d
atikas <robertas@admis.lt>
2024-02-05 22:30:44 +0200

    some documentation

--- back-end/src/TodoApp.API/Validators/GreaterOrEqualToTodayAttribute.cs
+++ back-end/src/TodoApp.API/Validators/GreaterOrEqualToTodayAttribute.cs
@@ -2,21 +2,26 @@
 
 namespace TodoApp.API.Validators;
 
+/// <summary>
+/// Validation attribute to ensure a date is greater than or equal to today.
+/// </summary>
 public class GreaterOrEqualToTodayAttribute : ValidationAttribute
 {
+    /// <summary>
+    /// Validates the input value to ensure it is a date greater than or equal to today.
+    /// </summary>
+    /// <param name="value">The input value to validate.</param>
+    /// <param name="validationContext">The context information about the validation operation.</param>
+    /// <returns>A ValidationResult object that encapsulates the information about the validation operation.</returns>
     protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
     {
         if (value == null || string.IsNullOrEmpty(value.ToString()))
-        {
             return ValidationResult.Success!;
-        }
 
         var date = DateTime.Parse(value!.ToString());
 
         if (date < DateTime.Today)
-        {
             return new ValidationResult("Date must be greater than or equal to today.");
-        }
 
         return ValidationResult.Success!;
     }
